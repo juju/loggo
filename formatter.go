ericsnowcurrently/juju-loggo

Fix backward compatibility.

formatter.go
@@ -4,8 +4,6 @@
 package loggo
 
 import (
-	"fmt"
-	"path/filepath"
 	"time"
 )
 
@@ -15,21 +13,8 @@ type Formatter interface {
 	Format(Record) string
 }
 
-// DefaultFormatter provides a simple concatenation of all the components.
-type DefaultFormatter struct{}
-
-// Format returns the parameters separated by spaces except for filename and
-// line which are separated by a colon.  The timestamp is shown to second
-// resolution in UTC.
-func (*DefaultFormatter) Format(rec Record) string {
-	ts := rec.Timestamp.In(time.UTC).Format("2006-01-02 15:04:05")
-	// Just get the basename from the filename
-	filename := filepath.Base(rec.Filename)
-	return fmt.Sprintf("%s %s %s %s:%d %s", ts, rec.Level, rec.Module, filename, rec.Line, rec.Message)
-}
-
-// TODO(ericsnow) The remainder of this file can go away when we fix
-// the NewSimpleWriter() signature.
+// TODO(ericsnow) The remainder of this file can go away when
+// NewSimpleWriter() does.
 
 // LegacyFormatter defines the single method Format, which takes the logging
 // information, and converts it to a string.
@@ -43,4 +28,24 @@ type legacyAdaptingFormatter struct {
 
 func (f *legacyAdaptingFormatter) Format(rec Record) string {
 	return f.legacy.Format(rec.Level, rec.Module, rec.Filename, rec.Line, rec.Timestamp, rec.Message)
+}
+
+// DefaultFormatter provides a simple concatenation of all the components.
+//
+// DefaultFormatter is deprecated. Pass nil to NewFormattingWriter() instead.
+type DefaultFormatter struct{}
+
+// Format returns the parameters separated by spaces except for filename and
+// line which are separated by a colon.  The timestamp is shown to second
+// resolution in UTC.
+func (*DefaultFormatter) Format(level Level, module, filename string, line int, timestamp time.Time, message string) string {
+	rec := Record{
+		Level:     level,
+		Module:    module,
+		Filename:  filename,
+		Line:      line,
+		Timestamp: timestamp,
+		Message:   message,
+	}
+	return rec.String()
 }

record.go
@@ -5,6 +5,7 @@ package loggo
 
 import (
 	"fmt"
+	"path/filepath"
 	"runtime"
 	"time"
 )
@@ -75,4 +76,15 @@ func NewRecordf(calldepth int, level Level, module, message string, args ...inte
 	rec.Message = fmt.Sprintf(rec.Message, args...)
 
 	return rec
+}
+
+// String returns the default string representation of the log record.
+// The details are separated by spaces except for filename and line
+// which are separated by a colon. The timestamp is shown to second
+// resolution in UTC.
+func (rec Record) String() string {
+	ts := rec.Timestamp.In(time.UTC).Format("2006-01-02 15:04:05")
+	// Just get the basename from the filename
+	filename := filepath.Base(rec.Filename)
+	return fmt.Sprintf("%s %s %s %s:%d %s", ts, rec.Level, rec.Module, filename, rec.Line, rec.Message)
 }

writer.go
@@ -6,32 +6,79 @@ package loggo
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 // defaultWriterName is the name of the writer default writer.
 const defaultWriterName = "default"
 
+// TODO(ericsnow) Drop legacyWriter once it's no longer used.
+
 // Writer is implemented by any recipient of log messages.
-type Writer interface {
+//
+// Writer is deprecated. Use RecordWriter instead.
+type Writer legacyWriter
+
+type legacyWriter interface {
 	// Write writes a message to the Writer with the given
+	// level and module name. The filename and line hold
+	// the file name and line number of the code that is
+	// generating the log message; the time stamp holds
+	// the time the log message was generated, and
+	// message holds the log message itself.
+	Write(level Level, name, filename string, line int, timestamp time.Time, message string)
+}
+
+// LegacyCompatibleWriter is a shim to temporarily support both interfaces.
+type LegacyCompatibleWriter interface {
+	RecordWriter
+	legacyWriter
+}
+
+// RecordWriter is implemented by any recipient of log messages.
+type RecordWriter interface {
+	// WriteRecord writes a message to the Writer for the given
 	// log record.
-	Write(Record)
+	WriteRecord(Record)
+}
+
+type legacyWriterShim struct {
+	RecordWriter
+}
+
+func (shim legacyWriterShim) Write(level Level, module, filename string, line int, timestamp time.Time, message string) {
+	shim.WriteRecord(Record{
+		Level:     level,
+		Module:    module,
+		Filename:  filename,
+		Line:      line,
+		Timestamp: timestamp,
+		Message:   message,
+	})
+}
+
+type legacyAdaptingWriter struct {
+	legacyWriter
+}
+
+func (law *legacyAdaptingWriter) WriteRecord(rec Record) {
+	law.Write(rec.Level, rec.Module, rec.Filename, rec.Line, rec.Timestamp, rec.Message)
 }
 
 // MinLevelWriter is a writer that exposes its minimum log level.
 type MinLevelWriter interface {
-	Writer
+	RecordWriter
 	HasMinLevel
 }
 
 type minLevelWriter struct {
-	writer Writer
+	writer RecordWriter
 	level  Level
 }
 
 // NewMinLevelWriter returns a MinLevelWriter that wraps the given
 // writer with the provided min log level.
-func NewMinLevelWriter(writer Writer, minLevel Level) MinLevelWriter {
+func NewMinLevelWriter(writer RecordWriter, minLevel Level) MinLevelWriter {
 	return &minLevelWriter{
 		writer: writer,
 		level:  minLevel,
@@ -44,11 +91,11 @@ func (w minLevelWriter) MinLogLevel() Level {
 }
 
 // Write writes the log record.
-func (w minLevelWriter) Write(rec Record) {
+func (w minLevelWriter) WriteRecord(rec Record) {
 	if !IsLevelEnabled(&w, rec.Level) {
 		return
 	}
-	w.writer.Write(rec)
+	w.writer.WriteRecord(rec)
 }
 
 // TODO(ericsnow) Eliminate NewSimpleWriter().
@@ -56,26 +103,37 @@ func (w minLevelWriter) Write(rec Record) {
 // NewSimpleWriter returns a new writer that writes
 // log messages to the given io.Writer formatting the
 // messages with the given formatter.
-func NewSimpleWriter(writer io.Writer, formatter LegacyFormatter) Writer {
-	return &formattingWriter{writer, &legacyAdaptingFormatter{formatter}}
+func NewSimpleWriter(writer io.Writer, formatter LegacyFormatter) LegacyCompatibleWriter {
+	return &legacyWriterShim{
+		&formattingWriter{writer, &legacyAdaptingFormatter{formatter}},
+	}
 }
 
+// formattingWriter is a log writer that writes
+// log messages to the given io.Writer, formatting the
+// messages with the given formatter.
 type formattingWriter struct {
 	writer    io.Writer
 	formatter Formatter
 }
 
 // NewFormattingWriter returns a new writer that writes
-// log messages to the given io.Writer formatting the
+// log messages to the given io.Writer, formatting the
 // messages with the given formatter.
-func NewFormattingWriter(writer io.Writer, formatter Formatter) Writer {
+func NewFormattingWriter(writer io.Writer, formatter Formatter) RecordWriter {
 	return &formattingWriter{
 		writer:    writer,
 		formatter: formatter,
 	}
 }
 
-func (fw *formattingWriter) Write(rec Record) {
-	logLine := fw.formatter.Format(rec)
+// Write formats the record and writes the result to the io.Writer.
+func (fw *formattingWriter) WriteRecord(rec Record) {
+	var logLine string
+	if fw.formatter == nil {
+		logLine = rec.String()
+	} else {
+		logLine = fw.formatter.Format(rec)
+	}
 	fmt.Fprintln(fw.writer, logLine)
 }

juju/loggo

formatter.go
@@ -4,8 +4,6 @@
 package loggo
 
 import (
-	"fmt"
-	"path/filepath"
 	"time"
 )
 
@@ -15,21 +13,8 @@ type Formatter interface {
 	Format(Record) string
 }
 
-// DefaultFormatter provides a simple concatenation of all the components.
-type DefaultFormatter struct{}
-
-// Format returns the parameters separated by spaces except for filename and
-// line which are separated by a colon.  The timestamp is shown to second
-// resolution in UTC.
-func (*DefaultFormatter) Format(rec Record) string {
-	ts := rec.Timestamp.In(time.UTC).Format("2006-01-02 15:04:05")
-	// Just get the basename from the filename
-	filename := filepath.Base(rec.Filename)
-	return fmt.Sprintf("%s %s %s %s:%d %s", ts, rec.Level, rec.LoggerName, filename, rec.Line, rec.Message)
-}
-
-// TODO(ericsnow) The remainder of this file can go away when we fix
-// the NewSimpleWriter() signature.
+// TODO(ericsnow) The remainder of this file can go away when
+// NewSimpleWriter() does.
 
 // LegacyFormatter defines the single method Format, which takes the logging
 // information, and converts it to a string.
@@ -43,4 +28,24 @@ type legacyAdaptingFormatter struct {
 
 func (f *legacyAdaptingFormatter) Format(rec Record) string {
 	return f.legacy.Format(rec.Level, rec.LoggerName, rec.Filename, rec.Line, rec.Timestamp, rec.Message)
+}
+
+// DefaultFormatter provides a simple concatenation of all the components.
+//
+// DefaultFormatter is deprecated. Pass nil to NewFormattingWriter() instead.
+type DefaultFormatter struct{}
+
+// Format returns the parameters separated by spaces except for filename and
+// line which are separated by a colon.  The timestamp is shown to second
+// resolution in UTC.
+func (*DefaultFormatter) Format(level Level, loggerName, filename string, line int, timestamp time.Time, message string) string {
+	rec := Record{
+		Level:      level,
+		LoggerName: loggerName,
+		Filename:   filename,
+		Line:       line,
+		Timestamp:  timestamp,
+		Message:    message,
+	}
+	return rec.String()
 }

record.go
@@ -5,6 +5,7 @@ package loggo
 
 import (
 	"fmt"
+	"path/filepath"
 	"runtime"
 	"time"
 )
@@ -77,4 +78,15 @@ func NewRecordf(calldepth int, level Level, loggerName, message string, args ...
 	rec.Message = fmt.Sprintf(rec.Message, args...)
 
 	return rec
+}
+
+// String returns the default string representation of the log record.
+// The details are separated by spaces except for filename and line
+// which are separated by a colon. The timestamp is shown to second
+// resolution in UTC.
+func (rec Record) String() string {
+	ts := rec.Timestamp.In(time.UTC).Format("2006-01-02 15:04:05")
+	// Just get the basename from the filename
+	filename := filepath.Base(rec.Filename)
+	return fmt.Sprintf("%s %s %s %s:%d %s", ts, rec.Level, rec.LoggerName, filename, rec.Line, rec.Message)
 }

writer.go
@@ -6,32 +6,79 @@ package loggo
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 // defaultWriterName is the name of the writer default writer.
 const defaultWriterName = "default"
 
+// TODO(ericsnow) Drop legacyWriter once it's no longer used.
+
 // Writer is implemented by any recipient of log messages.
-type Writer interface {
+//
+// Writer is deprecated. Use RecordWriter instead.
+type Writer legacyWriter
+
+type legacyWriter interface {
 	// Write writes a message to the Writer with the given
+	// level and module name. The filename and line hold
+	// the file name and line number of the code that is
+	// generating the log message; the time stamp holds
+	// the time the log message was generated, and
+	// message holds the log message itself.
+	Write(level Level, name, filename string, line int, timestamp time.Time, message string)
+}
+
+// LegacyCompatibleWriter is a shim to temporarily support both interfaces.
+type LegacyCompatibleWriter interface {
+	RecordWriter
+	legacyWriter
+}
+
+// RecordWriter is implemented by any recipient of log messages.
+type RecordWriter interface {
+	// WriteRecord writes a message to the Writer for the given
 	// log record.
-	Write(Record)
+	WriteRecord(Record)
+}
+
+type legacyWriterShim struct {
+	RecordWriter
+}
+
+func (shim legacyWriterShim) Write(level Level, loggerName, filename string, line int, timestamp time.Time, message string) {
+	shim.WriteRecord(Record{
+		Level:      level,
+		LoggerName: loggerName,
+		Filename:   filename,
+		Line:       line,
+		Timestamp:  timestamp,
+		Message:    message,
+	})
+}
+
+type legacyAdaptingWriter struct {
+	legacyWriter
+}
+
+func (law *legacyAdaptingWriter) WriteRecord(rec Record) {
+	law.Write(rec.Level, rec.LoggerName, rec.Filename, rec.Line, rec.Timestamp, rec.Message)
 }
 
 // MinLevelWriter is a writer that exposes its minimum log level.
 type MinLevelWriter interface {
-	Writer
+	RecordWriter
 	HasMinLevel
 }
 
 type minLevelWriter struct {
-	writer Writer
+	writer RecordWriter
 	level  Level
 }
 
 // NewMinLevelWriter returns a MinLevelWriter that wraps the given
 // writer with the provided min log level.
-func NewMinLevelWriter(writer Writer, minLevel Level) MinLevelWriter {
+func NewMinLevelWriter(writer RecordWriter, minLevel Level) MinLevelWriter {
 	return &minLevelWriter{
 		writer: writer,
 		level:  minLevel,
@@ -44,11 +91,11 @@ func (w minLevelWriter) MinLogLevel() Level {
 }
 
 // Write writes the log record.
-func (w minLevelWriter) Write(rec Record) {
+func (w minLevelWriter) WriteRecord(rec Record) {
 	if !IsLevelEnabled(&w, rec.Level) {
 		return
 	}
-	w.writer.Write(rec)
+	w.writer.WriteRecord(rec)
 }
 
 // TODO(ericsnow) Eliminate NewSimpleWriter().
@@ -56,26 +103,37 @@ func (w minLevelWriter) Write(rec Record) {
 // NewSimpleWriter returns a new writer that writes
 // log messages to the given io.Writer formatting the
 // messages with the given formatter.
-func NewSimpleWriter(writer io.Writer, formatter LegacyFormatter) Writer {
-	return &formattingWriter{writer, &legacyAdaptingFormatter{formatter}}
+func NewSimpleWriter(writer io.Writer, formatter LegacyFormatter) LegacyCompatibleWriter {
+	return &legacyWriterShim{
+		&formattingWriter{writer, &legacyAdaptingFormatter{formatter}},
+	}
 }
 
+// formattingWriter is a log writer that writes
+// log messages to the given io.Writer, formatting the
+// messages with the given formatter.
 type formattingWriter struct {
 	writer    io.Writer
 	formatter Formatter
 }
 
 // NewFormattingWriter returns a new writer that writes
-// log messages to the given io.Writer formatting the
+// log messages to the given io.Writer, formatting the
 // messages with the given formatter.
-func NewFormattingWriter(writer io.Writer, formatter Formatter) Writer {
+func NewFormattingWriter(writer io.Writer, formatter Formatter) RecordWriter {
 	return &formattingWriter{
 		writer:    writer,
 		formatter: formatter,
 	}
 }
 
-func (fw *formattingWriter) Write(rec Record) {
-	logLine := fw.formatter.Format(rec)
+// Write formats the record and writes the result to the io.Writer.
+func (fw *formattingWriter) WriteRecord(rec Record) {
+	var logLine string
+	if fw.formatter == nil {
+		logLine = rec.String()
+	} else {
+		logLine = fw.formatter.Format(rec)
+	}
 	fmt.Fprintln(fw.writer, logLine)
 }