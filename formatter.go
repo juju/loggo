ericsnowcurrently/juju-loggo

Add a Record type.

formatter.go
@@ -10,9 +10,9 @@ import (
 )
 
 // Formatter defines the single method Format, which takes the logging
-// information, and converts it to a string.
+// record and converts it to a string.
 type Formatter interface {
-	Format(level Level, module, filename string, line int, timestamp time.Time, message string) string
+	Format(Record) string
 }
 
 // DefaultFormatter provides a simple concatenation of all the components.
@@ -21,9 +21,26 @@ type DefaultFormatter struct{}
 // Format returns the parameters separated by spaces except for filename and
 // line which are separated by a colon.  The timestamp is shown to second
 // resolution in UTC.
-func (*DefaultFormatter) Format(level Level, module, filename string, line int, timestamp time.Time, message string) string {
-	ts := timestamp.In(time.UTC).Format("2006-01-02 15:04:05")
+func (*DefaultFormatter) Format(rec Record) string {
+	ts := rec.Timestamp.In(time.UTC).Format("2006-01-02 15:04:05")
 	// Just get the basename from the filename
-	filename = filepath.Base(filename)
-	return fmt.Sprintf("%s %s %s %s:%d %s", ts, level, module, filename, line, message)
+	filename := filepath.Base(rec.Filename)
+	return fmt.Sprintf("%s %s %s %s:%d %s", ts, rec.Level, rec.Module, filename, rec.Line, rec.Message)
+}
+
+// TODO(ericsnow) The remainder of this file can go away when we fix
+// the NewSimpleWriter() signature.
+
+// LegacyFormatter defines the single method Format, which takes the logging
+// information, and converts it to a string.
+type LegacyFormatter interface {
+	Format(level Level, module, filename string, line int, timestamp time.Time, message string) string
+}
+
+type legacyAdaptingFormatter struct {
+	legacy LegacyFormatter
+}
+
+func (f *legacyAdaptingFormatter) Format(rec Record) string {
+	return f.legacy.Format(rec.Level, rec.Module, rec.Filename, rec.Line, rec.Timestamp, rec.Message)
 }

formatter_test.go
@@ -20,6 +20,15 @@ func (*formatterSuite) TestDefaultFormat(c *gc.C) {
 	c.Assert(err, gc.IsNil)
 	testTime := time.Date(2013, 5, 3, 10, 53, 24, 123456, location)
 	formatter := &loggo.DefaultFormatter{}
-	formatted := formatter.Format(loggo.WARNING, "test.module", "some/deep/filename", 42, testTime, "hello world!")
-	c.Assert(formatted, gc.Equals, "2013-05-03 10:53:24 WARNING test.module filename:42 hello world!")
+
+	formatted := formatter.Format(loggo.Record{
+		Level:     loggo.WARNING,
+		Module:    "test.module",
+		Filename:  "some/deep/filename",
+		Line:      42,
+		Timestamp: testTime,
+		Message:   "hello world!",
+	})
+
+	c.Check(formatted, gc.Equals, "2013-05-03 10:53:24 WARNING test.module filename:42 hello world!")
 }

record.go
@@ -1,0 +1,78 @@
+// Copyright 2016 Canonical Ltd.
+// Licensed under the LGPLv3, see LICENCE file for details.
+
+package loggo
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+// Record holds the information for a single log record.
+type Record struct {
+	// Level is the log level of the log record.
+	Level Level
+
+	// Module is the name of the logger for which the record
+	// was generated.
+	Module string
+
+	// Filename is the path to the code that triggered the record.
+	Filename string
+
+	// Line is the line number of the code that triggered the record.
+	Line int
+
+	// Timestamp is the time that the record was created.
+	Timestamp time.Time
+
+	// Message is the requested log message.
+	Message string
+}
+
+// NewRecord creates a new log record for the given log level, module,
+// and message. It uses the identified entry the call stack to determine
+// the filename and line number. The current time is used for the
+// timestamp.
+func NewRecord(calldepth int, level Level, module, message string) Record {
+	// Gather time, and filename, line number.
+	now := time.Now() // get this early.
+	// Param to Caller is the call depth.  Since this method is called from
+	// the Logger methods, we want the place that those were called from.
+	_, file, line, ok := runtime.Caller(calldepth + 1)
+	if !ok {
+		file = "???"
+		line = 0
+	}
+	// Trim newline off format string, following usual
+	// Go logging conventions.
+	if len(message) > 0 && message[len(message)-1] == '\n' {
+		message = message[0 : len(message)-1]
+	}
+	return Record{
+		Level:     level,
+		Module:    module,
+		Filename:  file,
+		Line:      line,
+		Timestamp: now,
+		Message:   message,
+	}
+}
+
+// NewRecordf creates a new log record for the given info. The only
+// difference from NewRecord() is that the provided args are applied// to the message using fmt.Sprintf().
+func NewRecordf(calldepth int, level Level, module, message string, args ...interface{}) Record {
+	rec := NewRecord(calldepth+1, level, module, message)
+	if len(args) == 0 {
+		return rec
+	}
+
+	// To avoid having a proliferation of Info/Infof methods,
+	// only use Sprintf if there are any args, and rely on the
+	// `go vet` tool for the obvious cases where someone has forgotten
+	// to provide an arg.
+	rec.Message = fmt.Sprintf(rec.Message, args...)
+
+	return rec
+}

juju/loggo

formatter.go
@@ -10,9 +10,9 @@ import (
 )
 
 // Formatter defines the single method Format, which takes the logging
-// information, and converts it to a string.
+// record and converts it to a string.
 type Formatter interface {
-	Format(level Level, loggerName, filename string, line int, timestamp time.Time, message string) string
+	Format(Record) string
 }
 
 // DefaultFormatter provides a simple concatenation of all the components.
@@ -21,9 +21,26 @@ type DefaultFormatter struct{}
 // Format returns the parameters separated by spaces except for filename and
 // line which are separated by a colon.  The timestamp is shown to second
 // resolution in UTC.
-func (*DefaultFormatter) Format(level Level, loggerName, filename string, line int, timestamp time.Time, message string) string {
-	ts := timestamp.In(time.UTC).Format("2006-01-02 15:04:05")
+func (*DefaultFormatter) Format(rec Record) string {
+	ts := rec.Timestamp.In(time.UTC).Format("2006-01-02 15:04:05")
 	// Just get the basename from the filename
-	filename = filepath.Base(filename)
-	return fmt.Sprintf("%s %s %s %s:%d %s", ts, level, loggerName, filename, line, message)
+	filename := filepath.Base(rec.Filename)
+	return fmt.Sprintf("%s %s %s %s:%d %s", ts, rec.Level, rec.LoggerName, filename, rec.Line, rec.Message)
+}
+
+// TODO(ericsnow) The remainder of this file can go away when we fix
+// the NewSimpleWriter() signature.
+
+// LegacyFormatter defines the single method Format, which takes the logging
+// information, and converts it to a string.
+type LegacyFormatter interface {
+	Format(level Level, loggerName, filename string, line int, timestamp time.Time, message string) string
+}
+
+type legacyAdaptingFormatter struct {
+	legacy LegacyFormatter
+}
+
+func (f *legacyAdaptingFormatter) Format(rec Record) string {
+	return f.legacy.Format(rec.Level, rec.LoggerName, rec.Filename, rec.Line, rec.Timestamp, rec.Message)
 }

formatter_test.go
@@ -20,6 +20,15 @@ func (*formatterSuite) TestDefaultFormat(c *gc.C) {
 	c.Assert(err, gc.IsNil)
 	testTime := time.Date(2013, 5, 3, 10, 53, 24, 123456, location)
 	formatter := &loggo.DefaultFormatter{}
-	formatted := formatter.Format(loggo.WARNING, "test.module", "some/deep/filename", 42, testTime, "hello world!")
-	c.Assert(formatted, gc.Equals, "2013-05-03 10:53:24 WARNING test.module filename:42 hello world!")
+
+	formatted := formatter.Format(loggo.Record{
+		Level:      loggo.WARNING,
+		LoggerName: "test.module",
+		Filename:   "some/deep/filename",
+		Line:       42,
+		Timestamp:  testTime,
+		Message:    "hello world!",
+	})
+
+	c.Check(formatted, gc.Equals, "2013-05-03 10:53:24 WARNING test.module filename:42 hello world!")
 }

record.go
@@ -1,0 +1,80 @@
+// Copyright 2016 Canonical Ltd.
+// Licensed under the LGPLv3, see LICENCE file for details.
+
+package loggo
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+// Record holds the information for a single log record.
+type Record struct {
+	// Level is the log level of the log record.
+	Level Level
+
+	// LoggerName is the name of the logger for which the record
+	// was generated.
+	LoggerName string
+
+	// Filename is the path to the code that triggered the record.
+	Filename string
+
+	// Line is the line number of the code that triggered the record.
+	Line int
+
+	// Timestamp is the time that the record was created.
+	Timestamp time.Time
+
+	// Message is the requested log message.
+	Message string
+}
+
+// NewRecord creates a new log record for the given log level, logger name,
+// and message. It uses the identified entry the call stack to determine
+// the filename and line number. The current time is used for the
+// timestamp.
+func NewRecord(calldepth int, level Level, loggerName, message string) Record {
+	// Gather time, filename, and line number.
+	now := time.Now() // get this early.
+	// Param to Caller is the call depth.  Since this method is called from
+	// the Logger methods, we want the place that those were called from.
+	_, file, line, ok := runtime.Caller(calldepth + 1)
+	if !ok {
+		file = "???"
+		line = 0
+	}
+
+	// Trim newline off format string, following usual
+	// Go logging conventions.
+	if len(message) > 0 && message[len(message)-1] == '\n' {
+		message = message[0 : len(message)-1]
+	}
+
+	return Record{
+		Level:      level,
+		LoggerName: loggerName,
+		Filename:   file,
+		Line:       line,
+		Timestamp:  now,
+		Message:    message,
+	}
+}
+
+// NewRecordf creates a new log record for the given info. The only
+// difference from NewRecord() is that the provided args are applied// to the message using fmt.Sprintf().
+func NewRecordf(calldepth int, level Level, loggerName, message string, args ...interface{}) Record {
+	rec := NewRecord(calldepth+1, level, loggerName, message)
+	if len(args) == 0 {
+		return rec
+	}
+
+	// To avoid having a proliferation of Info/Infof methods,
+	// only use Sprintf if there are any args, and rely on the
+	// `go vet` tool for the obvious cases where someone has forgotten
+	// to provide an arg.
+	rec.Message = fmt.Sprintf(rec.Message, args...)
+
+	return rec
+}